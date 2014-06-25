dd9c259d829bd8839050ed9c3d41d6b36cbd7203
Joseph Guillaume <josephguillaume@gmail.com>
2014-06-25 11:43:54 +0300

    Figure 11 unified legend

--- fig11.R
+++ fig11.R
@@ -1,26 +1,38 @@
 ## T7 MCMC
 
-leg.height<-1
+leg.height<-0.9
 #win.metafile("fig11 mcmc.wmf",width=7,height=3+leg.height)
 png("fig11 mcmc.png",width=7,height=5+leg.height,units="in",res=70)
 
-layout(matrix(c(1,2,3,4), nrow = 2), heights = c(leg.height/(3+leg.height), 3/(3+leg.height)))
+layout(matrix(c(1,2,1,3), nrow = 2), heights = c(leg.height/(3+leg.height), 3/(3+leg.height)))
 
 ##################################################################
-## Legend for posterior density plot
+## Legend
 ##################################################################
 df.leg=data.frame(
-  legend=c("Flood threshold\n  (normative boundary)\n",
-           "Confidence interval\n  (epistemic boundary)\n"
+  legend=c("Not physically possible",
+           "No flood occurs", 
+           "Flood occurs",
+           "Flood threshold\n  (normative boundary)\n",
+           "Confidence interval\n  (epistemic boundary)\n",
+           "Points in critical region",
+           "Points in confidence interval",
+           "Critical values"
   ),
-  lwd=c(2,1),
-  lty=c(1,2),
-  col=c("grey","blue"),
+  fill=c("grey90","white","grey",rep(NA,2),NA,NA,NA),
+  density=c(NA,NA,25,rep(NA,2),NA,NA,NA),
+  angle=c(NA,NA,45,rep(NA,2),NA,NA,NA),
+  border=c(NA,"grey","grey",rep(NA,2),NA,NA,NA),
+  col=c(rep(NA,3),"grey","blue","red","grey50","blue"),
+  pch=c(rep(NA,3),rep(NA,2),1,19,3),
+  pt.cex=c(rep(NA,3),rep(NA,2),1,1,1.5),
+  lwd=c(rep(NA,3),2,1,rep(NA,3)),
+  lty=c(rep(NA,3),1,2,rep(NA,3)),
   stringsAsFactors=FALSE
 )
 par(mar=c(0,1.1,0,1.1),cex=0.8)
 plot.new()
-do.call(legend,modifyList(as.list(df.leg),list(x="bottom",ncol=1,bty="n")))
+do.call(legend,modifyList(as.list(df.leg),list(x="bottom",ncol=3,bty="n")))
 
 ##################################################################
 ## Posterior density plot
@@ -37,31 +49,6 @@ abline(v=quantile(ys,c((1 - level)/2,1 - (1 - level)/2)),lty=2,col="blue")
 abline(v=7.6,lwd=2,col="grey")
 mtext(" a) ",side=3,adj=0,line=-1,cex=0.8)
 points(lik.ht.pars.ys,c(0,0),pch=3,col="blue",cex=1.5)
-
-##################################################################
-## Legend for set diagram
-##################################################################
-
-df.leg=data.frame(
-  legend=c("Not physically possible",
-           "No flood occurs", 
-           "Flood occurs",
-           "Points in critical region",
-           "Points in confidence interval",
-           "Critical values"
-  ),
-  fill=c("grey90","white","grey",NA,NA,NA),
-  density=c(NA,NA,25,NA,NA,NA),
-  angle=c(NA,NA,45,NA,NA,NA),
-  border=c(NA,"grey","grey",NA,NA,NA),
-  col=c(NA,NA,NA,"red","grey50","blue"),
-  pch=c(rep(NA,3),1,19,3), #3 should be +
-  pt.cex=c(rep(NA,3),1,1,1.5),
-  stringsAsFactors=FALSE
-)
-par(mar=c(0,1.1,0,1.1),cex=0.8)
-plot.new()
-do.call(legend,modifyList(as.list(df.leg),list(x="bottom",ncol=1,bty="n")))
 
 ##################################################################
 ## Normative and epistemic set diagrams in parameter space
